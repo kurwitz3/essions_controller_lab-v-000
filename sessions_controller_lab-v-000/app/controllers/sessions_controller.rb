5280262d9f81881617606d299bbd4c7b3f543a7c
Richard Kurwitz <kurwitz3@aol.com>
2021-05-06 19:58:10 -0400

    Add files via upload

---
+++ sessions_controller_lab-v-000/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name] != nil
+      session[:name] = params[:name]
+    end
+    if params[:name] == nil || params[:name] == ''
+      redirect_to login_path
+    else
+    redirect_to root_path
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.delete :name
+    end
+  end
+end
